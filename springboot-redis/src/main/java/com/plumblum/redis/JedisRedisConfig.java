0b7a0a09cedb720c4447f55312609fe0d4d419d5
Plumblumpb <2573350997@qq.com>
2018-08-03 17:51:52 +0800

    1.redis_配置。

---
+++ springboot-redis/src/main/java/com/plumblum/redis/JedisRedisConfig.java
@@ -0,0 +1,48 @@
+package com.plumblum.redis;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+/**
+ * @Auther: cpb
+ * @Date: 2018/8/3 17:44
+ * @Description:
+ */
+@Configuration
+public class JedisRedisConfig {
+    @Value("${spring.redis.host}")
+    private  String host;
+    @Value("${spring.redis.password}")
+    private  String password;
+    @Value("${spring.redis.port}")
+    private  int port;
+    @Value("${spring.redis.timeout}")
+    private  int timeout;
+    @Value("${spring.redis.jedis.pool.max-idle}")
+    private int maxIdle;
+    @Value("${spring.redis.jedis.pool.max-wait}")
+    private long maxWaitMillis;
+
+    @Bean
+    JedisConnectionFactory jedisConnectionFactory() {
+        JedisConnectionFactory factory = new JedisConnectionFactory();
+        factory.setHostName(host);
+        factory.setPort(port);
+        factory.setTimeout(timeout);
+        factory.setPassword(password);
+        return factory;
+    }
+    @Bean
+    public JedisPool redisPoolFactory() {
+        JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
+        jedisPoolConfig.setMaxIdle(maxIdle);
+        jedisPoolConfig.setMaxWaitMillis(maxWaitMillis);
+
+        JedisPool jedisPool = new JedisPool(jedisPoolConfig, host, port, timeout, password);
+        return jedisPool;
+    }
+
+}
---
+++ springboot-redis/src/test/java/com/plumblum/redis/RedisTest.java
@@ -0,0 +1,49 @@
+package com.plumblum.redis;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @Auther: cpb
+ * @Date: 2018/8/3 17:10
+ * @Description:
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RedisTest {
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
+
+    @Test
+    public void test() throws Exception {
+
+        RedisObjectSerializer redisObjectSerializer = new RedisObjectSerializer();
+
+        // 保存字符串
+        stringRedisTemplate.opsForValue().set("aaa", "111");
+        Assert.assertEquals("111", stringRedisTemplate.opsForValue().get("aaa"));
+
+        // 保存对象
+        User user = new User("超人", 20);
+        redisObjectSerializer.serialize(user);
+        redisTemplate.opsForValue().set(user.getUsername(), user);
+
+        user = new User("蝙蝠侠", 30);
+        redisTemplate.opsForValue().set(user.getUsername(), user);
+
+        user = new User("蜘蛛侠", 40);
+        redisTemplate.opsForValue().set(user.getUsername(), user);
+
+
+    }
+}
+
