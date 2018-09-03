7c5f8637397096af37e6fc1e525a5422799f243b
Plumblumpb <2573350997@qq.com>
2018-09-03 20:08:36 +0800

    1.文件上传。

---
+++ springboot-fileupload/src/main/java/com/plumblum/fileconfig/FileConfig.java
@@ -0,0 +1,22 @@
+package com.plumblum.fileconfig;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+
+/**
+ * @Auther: cpb
+ * @Date: 2018/9/3 20:04
+ * @Description:
+ */
+@Configuration
+public class FileConfig {
+    @Bean
+
+    public MultipartResolver multipartResolver(){
+        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+        multipartResolver.setMaxUploadSize(1000000);
+        return  multipartResolver;
+    }
+}
